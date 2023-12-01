f8ff75d5b91457afaa68804bbe590cfe747154f2
JairoWarner <91611684+JairoWarner@users.noreply.github.com>
2023-12-01 12:01:33 +0100

    symbols maded Jairo

--- assets/nav.php
+++ assets/nav.php
@@ -1,90 +1,80 @@
+<!DOCTYPE html>
+<html>
 
-<nav>
-    <div class="logo">
-        <a class="bas" href="index">Stadsklacht</a>
-        <p class="bbb">Voor een leefbare Rotterdam</p>
-        <!-- <img src="img/basLogo.png" alt="Bas Logo"> -->
+<head>
+    <!-- Basic -->
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <!-- Mobile Metas -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <!-- Site Metas -->
+    <meta name="keywords" content="" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+
+    <link href="bootstrap.css" rel="stylesheet" />
+    <link rel="stylesheet" href="main.css">
+    <link rel="stylesheet" href="responsive.css">
+    <link rel="stylesheet" href="style.map.css">
+    <link rel="stylesheet" href="style.scss">
+    <link rel="stylesheet" href="style1.css">
+
+
+</head>
+<body>
+<header class="header_section">
+    <div class="container-fluid">
+
     </div>
 
-    <div class="navContainer">
-        <div> <a href="index" class="navLink">Home</a>
-            <?php
-                require 'database/database.php';
+<nav class="navbar navbar-expand-lg custom_nav-container ">
+    <a class="navbar-brand" href="../route.php">
+        <img src="images/logo.png" alt="">
+        <span>
+              ELEMENTS
+            </span>
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
 
-                // Check if user is logged in
-                if(isset($_SESSION['username'])) {
-                    require 'function.php';
-                
-
-                    // Display different navigation bar based on user's 'functie'
-                    switch($rights) {
-                        case "admin":
-                            echo '<a href="registerForm" class="navLink">Register</a>'; 
-                            echo '<div class="dropdown">';
-                            echo '<button class="dropbtn">Management Systeem</button>';
-                            echo '<div class="dropdown-content">';
-                            echo '<a href="klantReadManagement" class="navLink">Klant</a>'; 
-                            echo '<a href="boekingReadManagement" class="navLink">Boekingen</a>'; 
-                            echo '</div>';
-                            echo '</div>';
-                            echo '<div class="dropdown">';
-                            echo '<button class="dropbtn">Admin</button>';
-                            echo '<div class="dropdown-content">';
-                            echo '<a href="adminTableBoekingen" class="navLink">Boekingen</a>'; 
-                            echo '<a href="adminTableTochten" class="navLink">Tochten</a>'; 
-                            echo '<a href="adminTableKlanten" class="navLink">Klanten</a>'; 
-                            echo '<a href="adminTableStatussen" class="navLink">Statussen</a>'; 
-                            echo '</div>';
-                            echo '</div>';
-                            break;
-                        case "management":
-                            // echo '<a href="registerForm" class="navLink">Register</a>'; 
-                            echo '<div class="dropdown">';
-                            echo '<button class="dropbtn">Management Systeem</button>';
-                            echo '<div class="dropdown-content">';
-                            echo '<a href="klantReadManagement" class="navLink">Klant</a>'; 
-                            echo '<a href="boekingReadManagement" class="navLink">Boekingen</a>'; 
-                            echo '</div>';
-                            echo '</div>';
-                            break;
-                        case 'NULL'|| NULL:
-                            // Display navigation bar for klant
-                            echo '<a href="about" class="navLink">About</a>'; 
-                            echo '<a href="contact" class="navLink">Contact</a>'; 
-                            echo '<div class="dropdown">';
-                            echo '<button class="dropbtn">Boekingen</button>';
-                            echo '<div class="dropdown-content">';
-                            echo '<a href="boekingRead" class="navLink">Inzien</a>'; 
-                            echo '<a href="boekingCreateForm" class="navLink">Aanvragen</a>'; 
-                            echo '</div>';
-                            echo '</div>';     
-                            break;
-                    default:
-                        // Display default navigation bar
-                        break;
-                    }
-
-                } else {
-
-                    // Display navigation bar for non-logged-in users
-                    ?>
-                    <a href="about" class="navLink">About</a>
-                    <a href="contact" class="navLink">Contact</a>
-                    <?php
-
-                }
-
-            ?>
-        </div>  
-    </div>
-    <div class="navLogin">
-        <?php if(isset($_SESSION['username'])):?>
-            <a href="account" class="navLinkLogin"><?php echo $_SESSION['username']; ?>: <i class='bx bxs-user-detail' ></i></a>
-            <a href="logout" class="navLinkLogin">Logout: <i class='bx bxs-user-minus' ></i></a>
-            <?php else: ?>
-            <a href="loginForm" class="navLinkLogin">Login: <i class='bx bxs-user' ></i></a>
-            <a href="registerForm" class="navLinkLogin">Register: <i class='bx bxs-user-plus'></i></a>
-        <?php endif; ?>
- 
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <div class="d-flex mx-auto flex-column flex-lg-row align-items-center">
+            <ul class="navbar-nav  ">
+                <li class="nav-item active">
+                    <a class="nav-link" href="../route.php">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="../route.php">About </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="../route.php">Services </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="../route.php"> Portfolio</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="../route.php"> news </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="../route.php">Contact us</a>
+                </li>
+            </ul>
+        </div>
+        <div class="quote_btn-container  d-flex justify-content-center">
+            <a href="">
+                <img src="images/call.png" alt="">
+                CALL : +01 1234567890
+            </a>
+            <a href="">
+                <span>
+                  @
+                </span>
+                demo@gmail.com
+            </a>
+        </div>
     </div>
 </nav>
+</header>
+</body>
