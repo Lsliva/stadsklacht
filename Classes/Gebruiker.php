73ea21667e580fa34fe0cadfb626b0a5c2ff8125
JoranBoonstoppel <boonstoppeljoran@gmail.com>
2024-01-18 10:39:33 +0100

    gebruiker class

--- Classes/Gebruiker.php
+++ Classes/Gebruiker.php
@@ -24,6 +24,41 @@ class Gebruiker {
     public function setWachtwoord($wachtwoord) {
         $this->wachtwoord = $wachtwoord;
     }
+
+
+// get the user information using gebruikerId for gps pins
+public function getGebruiker($gebruikersId) {
+    require 'database/conn.php';
+
+    $statement = $conn->prepare("SELECT naam, email FROM gebruikers WHERE ID = :gebruikersId");
+    $statement->bindParam(':gebruikersId', $gebruikersId, PDO::PARAM_INT);
+    $statement->execute();
+    $results = $statement->fetch(PDO::FETCH_ASSOC);
+
+    return $results;
+}
+
+
+public function getKlantIdSession($qqleq) {
+
+    require_once 'database/conn.php';
+    $sql = $conn->prepare('SELECT id FROM gebruikers WHERE naam = :username');
+    $sql->bindParam(':username', $qqleq);
+
+    $sql->execute();
+
+    $row = $sql->fetch(PDO::FETCH_ASSOC);
+
+
+    if ($row) {
+        return $row['id'];
+    } else {
+        return null;
+
+    }
+}
+
+
 }
 
 ?>
